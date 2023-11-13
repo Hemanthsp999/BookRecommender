c4621db4dd646969d8a38abdc8e12f22f5c6b069
Hemantsp999 <hemanth.sp03@gmail.com>
2023-11-13 23:04:07 +0530

    Added NavBar for sm device but it's not responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
 import Alert from "./components/Alert";
-import ReadSomeBook from "./components/images/ReadBook.jpeg";
+import ReadSomeBook from "./components/images/BookMatch.png";
 
 function App() {
   const [jwtToken, setJwtToken] = useState(false);
@@ -21,13 +22,13 @@ function App() {
             <Link to="/">
               <img
                 className="img-fluid rounded"
-                style={{ width: "60px" }}
+                style={{ width: "100px" }}
                 src={ReadSomeBook}
                 alt="ReadSomeBook.jpg"
               />
             </Link>
             <span
-              className="mx-2 mt-3"
+              className="mx-2 mt-4"
               style={{
                 fontFamily: "serif",
                 fontWeight: "bold",
@@ -40,7 +41,7 @@ function App() {
             </span>
           </h1>
         </div>
-        <div className="col text-end mt-4">
+        <div className="col text-end mt-5">
           <Link to="/signup">
             {" "}
             <span className="badge bg-success mx-1">Sign Up</span>
@@ -58,49 +59,64 @@ function App() {
         <hr className="mb-3"></hr>
         <div className="row">
           <div className="col-md-2">
-            <nav>
-              <div className="list-group">
-                <Link to="/" className="list-group-item list-group-item-action">
-                  Home
-                </Link>
-                <Link
-                  to="/books"
-                  className="list-group-item list-group-item-action"
+            <Navbar
+              className="navbar navbar-dark bg-dark"
+              bg="dark"
+              variant="dark"
+              sticky="top"
+              expand="md"
+              collapseOnSelect
+            >
+              <Navbar.Toggle />
+              <Navbar.Collapse className="bg-white navbar-expand-lg">
+                <Nav>
+                  <div className="list-group">
+                    <Link
+                      to="/"
+                      className="list-group-item list-group-item-action"
+                    >
+                      Home
+                    </Link>
+                    <Link
+                      to="/books"
+                      className="list-group-item list-group-item-action"
+                    >
+                      Books
+                    </Link>
+                    {jwtToken === true && (
+                      <>
+                        <Link
+                          to="/genre"
+                          className="list-group-item list-group-item-action"
+                        >
+                          Genre
+                        </Link>
+                        <Link
+                          to="/Fav"
+                          className="list-group-item list-group-item-action"
+                        >
+                          Favourites
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <nav className="d-sm-navbar d-none navbar-dark bg-dark">
+              <div className="container">
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="CollapseNavBar"
+                  aria-controls="CollapseNavBar"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
                 >
-                  Books
-                </Link>
-                {jwtToken === true && (
-                  <>
-                    <Link
-                      to="/genre"
-                      className="list-group-item list-group-item-action"
-                    >
-                      Genre
-                    </Link>
-                    <Link
-                      to="/Fav"
-                      className="list-group-item list-group-item-action"
-                    >
-                      Favourites
-                    </Link>
-                  </>
-                )}
+                  <span className="navbar-toggler-icon"></span>
+                </button>
               </div>
-              <nav className="d-sm-navbar d-none navbar-dark bg-dark">
-                <div className="container">
-                  <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="CollapseNavBar"
-                    aria-controls="CollapseNavBar"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                  >
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                </div>
-              </nav>
             </nav>
           </div>
           <div className="col-md-10">
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import Lib from "./images/lib.jpg";
+
 const Home = () => {
   return (
     <>
-      <div className="text-center">
-        <h4 className="text-center">Find a Book to read</h4>
+      <div className="text-center mt-1">
+        <h4
+          className="text-center"
+          style={{ fontFamily: "monospace", fontWeight: "bold" }}
+        >
+          Feel Free to read a Book!..
+        </h4>
         <hr className="mb-1"></hr>
         <Link to="/books">
           <img
